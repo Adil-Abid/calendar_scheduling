d2544bc4bb41890539e628eed074b460f2561e6f
Adil-Abid <111174935+Adil-Abid@users.noreply.github.com>
2024-12-09 19:40:34 +0500

    mongoDB setup

--- config/app.php
+++ config/app.php
@@ -168,6 +168,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        MongoDB\Laravel\MongoDBServiceProvider::class,
+
+
+
     ])->toArray(),
 
     /*
